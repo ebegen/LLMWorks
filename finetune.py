7d80ffff43001b64a56c6dcb9ba5e857b57b680b
ebegen <ebegen51@gmail.com>
2023-10-25 01:42:27 -0700

    finetune.py created

---
+++ finetune.py
@@ -0,0 +1,24 @@
+from datasets import load_dataset, DatasetDict, Dataset
+
+from transformers import (
+    AutoTokenizer,
+    AutoConfig, 
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer)
+
+from peft import PeftModel, PeftConfig, get_peft_model, LoraConfig
+import evaluate
+import torch
+import numpy as np
+
+model_checkpoint = 'distilbert-base-uncased'
+
+# define label maps
+id2label = {0: "Negative", 1: "Positive"}
+label2id = {"Negative":0, "Positive":1}
+
+# generate classification model from model_checkpoint
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_checkpoint, num_labels=2, id2label=id2label, label2id=label2id)
