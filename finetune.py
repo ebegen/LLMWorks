08a33e157c0db220da7d1217796f9184bc76b1a9
ebegen <ebegen51@gmail.com>
2023-10-26 00:03:15 -0700

    tokenizer added

--- finetune.py
+++ finetune.py
@@ -22,3 +22,60 @@ label2id = {"Negative":0, "Positive":1}
 # generate classification model from model_checkpoint
 model = AutoModelForSequenceClassification.from_pretrained(
     model_checkpoint, num_labels=2, id2label=id2label, label2id=label2id)
+
+# load dataset
+dataset = load_dataset("shawhin/imdb-truncated")
+dataset
+
+# dataset = 
+# DatasetDict({
+#     train: Dataset({
+#         features: ['label', 'text'],
+#         num_rows: 1000
+#     })
+#     validation: Dataset({
+#         features: ['label', 'text'],
+#         num_rows: 1000
+#     })
+# }) 
+
+# create tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, add_prefix_space=True)
+
+# create tokenize function
+def tokenize_function(examples):
+    # extract text
+    text = examples["text"]
+
+    #tokenize and truncate text
+    tokenizer.truncation_side = "left"
+    tokenized_inputs = tokenizer(
+        text,
+        return_tensors="np",
+        truncation=True,
+        max_length=512
+    )
+
+    return tokenized_inputs
+
+
+# add pad token if none exists
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    model.resize_token_embeddings(len(tokenizer))
+
+# tokenize training and validation datasets
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+tokenized_dataset
+
+# tokenized_dataset = 
+# DatasetDict({
+#     train: Dataset({
+#        features: ['label', 'text', 'input_ids', 'attention_mask'],
+#         num_rows: 1000
+#     })
+#     validation: Dataset({
+#         features: ['label', 'text', 'input_ids', 'attention_mask'],
+#         num_rows: 1000
+#     })
+# })
